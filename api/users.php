2c86ded97c392be1519a2b913a4f0042391bfde3
xseko60 tr60 <158826606+WormateSerkanConnect@users.noreply.github.com>
2025-01-11 21:22:49 +0300

    Update users.php

--- api/users.php
+++ api/users.php
@@ -1,1 +1,12 @@
-{ "id": 61, "cliente_NOMBRE": "Sekosaglm", "cliente_ID": "gg_106263452150533879793", "Client_KeyAccecs": "XTPRIVATESKIN", "cliente_DateExpired": "2024-12-20", "status": 1, "Client_VisibleSkin": "0000", "Client_VisibleSkin1": "0000", "Client_VisibleSkin2": "0000", "Client_VisibleSkin3": "0000", "Client_VisibleSkin4": "0000", "Client_VisibleSkin5": "0000", "Client_VisibleSkin6": "0000", "Client_VisibleSkin7": "0000", "Client_VisibleSkin8": "0000", "Client_VisibleSkin9": "0000", "Client_VisibleSkin10": "0000" },
+{
+         "id":  353,
+         "cliente_NOMBRE":  "WSC DEVLOPER",
+         "cliente_ID":  "gg_106263452150533879793",
+         "Client_VisibleSkin":  "4790",
+         "Client_VisibleSkin1":  "4793",
+         "Client_VisibleSkin2":  "4797",
+         "Client_VisibleSkin3":  "4812",
+         "Client_KeyAccecs":  "XTPRIVATESKIN",
+         "cliente_DateExpired":  "2024-11-10",
+         "status":  1
+    },
