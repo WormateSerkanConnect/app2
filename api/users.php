99e8ac37e16b6d4b7a4cb30d256fe98a7096856d
xseko60 tr60 <158826606+WormateSerkanConnect@users.noreply.github.com>
2025-01-13 19:22:01 +0300

    Update users.php

--- api/users.php
+++ api/users.php
@@ -1,10 +1,13 @@
-        {
-            "id": 95,
-            "cliente_NOMBRE": "WSC DEVLOPER ",
-            "cliente_ID": "gg_106263452150533879793",
-            "Client_KeyAccecs": "XTPRIVATESKIN",
-            "cliente_DateStarted": "15-10-2024",
-            "cliente_DateExpired": "31-12-2028",
-            "cliente_sothang": 3,
-            "status": 1
-        },
+{
+  "success": true,
+  "Users":S[
+    {
+      "cliente_DateExpired": null,
+      "cliente_ID": gg_106263452150533879793,
+      "active": true,
+      "Client_VisibleSkin": true,
+      "Client_KeyAccecs": "XTPRIVATESKIN"
+    }
+  ]
+}
+1 : SERKO
